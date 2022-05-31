ae49fee7205af305d89826b1c49ca6f55f44fa6c
Charles Bousseau <cbousseau@anaconda.com>
2022-05-31 15:15:47 -0400

    build cairo-gobject on win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -13,6 +13,16 @@ make -f Makefile.win32 CFG=release ^
   LIBPNG_CFLAGS=-I%CYGWIN_PREFIX%/Library/include/ ^
   CAIRO_LIBS='gdi32.lib msimg32.lib user32.lib %CYGWIN_PREFIX%/Library/lib/libpng.lib %CYGWIN_PREFIX%/Library/lib/zlib.lib'
 if errorlevel 1 exit 1
+pushd util\cairo-gobject
+make -f Makefile.win32 CFG=release ^
+  PIXMAN_CFLAGS=-I%CYGWIN_PREFIX%/Library/include/pixman ^
+  PIXMAN_LIBS=%CYGWIN_PREFIX%/Library/lib/pixman-1.lib ^
+  ZLIB_CFLAGS=-I%CYGWIN_PREFIX%/Library/include/ ^
+  LIBPNG_CFLAGS=-I%CYGWIN_PREFIX%/Library/include/ ^
+  GOBJECT_CFLAGS='-I%CYGWIN_PREFIX%/Library/include/glib-2.0 -I%CYGWIN_PREFIX%/Library/lib/glib-2.0/include' ^
+  GOBJECT_LIBS='%CYGWIN_PREFIX%/Library/lib/glib-2.0.lib %CYGWIN_PREFIX%/Library/lib/gobject-2.0.lib'
+if errorlevel 1 exit 1
+popd
 
 :: Installing.
 set CAIRO_INC=%LIBRARY_INC%\cairo
@@ -26,7 +36,15 @@ move src\cairo-script.h %CAIRO_INC%
 move src\cairo-ps.h %CAIRO_INC%
 move src\cairo-pdf.h %CAIRO_INC%
 move src\cairo-svg.h %CAIRO_INC%
+move src\cairo-ft.h %CAIRO_INC%
+move util\cairo-gobject\cairo-gobject.h %CAIRO_INC%
 
 move src\release\cairo.dll %LIBRARY_BIN%
 move src\release\cairo.lib %LIBRARY_LIB%
 move src\release\cairo-static.lib %LIBRARY_LIB%
+move util\cairo-gobject\release\cairo-gobject.dll %LIBRARY_BIN%
+move util\cairo-gobject\release\cairo-gobject.lib %LIBRARY_LIB%
+
+for %%F in (pc-files\*.pc) do (
+  envsubst '${LIBRARY_PREFIX}${PKG_VERSION}' < %%F > %LIBRARY_LIB%\pkgconfig\%%~nF.pc
+)
