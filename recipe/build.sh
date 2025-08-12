820ec362293a2a8e309ce7b9181a8c487bb155dd
Ivan Iziumov <iiziumov@anaconda.com>
2025-08-12 14:50:02 +0200

    cairo 1.18.4

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,57 +1,41 @@
 #!/bin/bash
 set -u
-declare -a CONFIGURE_OTHER_ARGS
 
-# Get an updated config.sub and config.guess
-cp -r ${BUILD_PREFIX}/share/libtool/build-aux/config.* ./build
+mkdir -p build
 
-mkdir -p $SRC_DIR/cyclictool
-echo "#!/bin/bash" >$SRC_DIR/cyclictool/gtkdocize
-echo "" >>$SRC_DIR/cyclictool/gtkdocize
-echo "exit 0" >>$SRC_DIR/cyclictool/gtkdocize
-chmod +x $SRC_DIR/cyclictool/gtkdocize
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$BUILD_PREFIX/lib/pkgconfig
 
-export PATH=$SRC_DIR/cyclictool:$PATH
+export ADDITIONAL_MESON_ARGS=(
+  --buildtype=release
+  --prefix="${PREFIX}"
+  --default-library=shared
+  --wrap-mode=nofallback
+  -Dlibdir=lib
+  -Dfreetype=enabled
+  -Dfontconfig=enabled
+  -Dglib=enabled
+  -Dgtk_doc=false
+  # meson.build:602:7: ERROR: Program 'gs' not found or not executable - program needed during testing (not in main chanel)
+  # -Dtests=enabled
+)
 
 if [[ ${target_platform} == osx-* ]]; then
-  rm -rf "${PREFIX}"/lib/libuuid.la "${PREFIX}"/lib/libuuid.a
-  CONFIGURE_OTHER_ARGS=(--without-x)
+  ADDITIONAL_MESON_ARGS+=(-Dxlib=disabled)
 else
-  CONFIGURE_OTHER_ARGS=(--with-x)
-  CONFIGURE_OTHER_ARGS+=(--enable-xcb)
-
-  # Needed to find .pc files from CDTs
+  ADDITIONAL_MESON_ARGS+=(-Dxlib=enabled)
+  ADDITIONAL_MESON_ARGS+=(-Dxcb=enabled)
   : ${CONDA_BUILD_SYSROOT:=`"$CC" -print-sysroot`}
   export PKG_CONFIG_PATH="${CONDA_BUILD_SYSROOT}/usr/lib64/pkgconfig"
   if [[ ${target_platform} == linux-ppc64le ]]; then
-    # there are issues with CDTs and there HOST name ...
-    pushd "${BUILD_PREFIX}"
-    cp -Rn powerpc64le-conda_cos7-linux-gnu/* powerpc64le-conda-linux-gnu/.
-    popd
     export CFLAGS="${CFLAGS} -Wno-enum-conversion"
   fi
 fi
+
 if [ $(uname -m) == x86_64 ] || [ $(uname -m) == aarch64 ]; then
-    export ax_cv_c_float_words_bigendian="no"
+  export ax_cv_c_float_words_bigendian="no"
 fi
-bash autogen.sh
 
-find $PREFIX -name '*.la' -delete
-
-./configure \
-    --prefix="${PREFIX}" \
-    --enable-warnings \
-    --enable-ft \
-    --enable-ps \
-    --enable-pdf \
-    --enable-svg \
-    --disable-gtk-doc \
-    "${CONFIGURE_OTHER_ARGS[@]}"
-
-make -j${CPU_COUNT}
-# FAIL: check-link on OS X
-# Hangs for > 10 minutes on Linux
-#make check -j${CPU_COUNT}
-make install -j${CPU_COUNT}
-
-find $PREFIX -name '*.la' -delete
+meson setup build ${ADDITIONAL_MESON_ARGS[@]}
+meson compile -vC build -j ${CPU_COUNT}
+# meson test
+meson install -C build
